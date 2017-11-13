5d9e60e6d508146aca7f5449840122a7768de2d6
zhang156 <1028524971@qq.com>
2017-11-13 22:30:40 +0800

    add some css

--- components/asside.vue
+++ components/asside.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="asside">
     <!-- searchbar -->
-    <div class="searchbar">
+    <div class="search">
       <div class="search_content">
-        <input type="text" name="search_input" class="search_input">
-        <span>
+        <input type="search" name="search_input" class="search_input" placeholder="Search...">
+        <span class="searchbar">
           <i class="iconfont icon-sousuo"></i>
         </span>
       </div>
@@ -19,6 +19,9 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+  @import '~assets/sass/mixins.scss';
+  @import '~assets/sass/variables.scss';
+
   #asside {
     float: right;
     width: 19em;
@@ -26,18 +29,30 @@ export default {
     position: relative;
     z-index: 1;
 
-    .searchbar {
+    .search {
       padding: 5px 5px;
       background: #fff;
 
       .search_input {
         background-color: hsla(0,0%,77%,.4);
         height: 2em;
-        
+        line-height: 2em;
+        vertical-align: middle;
 
         &:hover {
           background-color: hsla(0,0%,57%,.4);
         }
+      }
+
+      .searchbar {
+        display: inline-block;
+        // font-size: 1.2em;
+        height: 2em;
+        width: 2em;
+        line-height: 2em;
+        text-align: center;
+        background-color: hsla(0,0%,57%,.4);
+        vertical-align: middle;
       }
     }
   }
