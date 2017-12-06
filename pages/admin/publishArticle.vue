96492009533849de42f943364deb3a6963d3063b
zhang156 <1028524971@qq.com>
2017-12-06 09:46:50 +0800

    update

---
+++ pages/admin/publishArticle.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="publishArticle">
+    <Article></Article>
+  </div>
+</template>
+
+<script>
+import Article from '~/components/article'
+
+export default {
+  name: 'publishArticle',
+  layout: 'admin',
+  components: { Article }  
+}
+</script>
+
+<style lang="scss" scoped>
+  #publishArticle {
+
+  }
+</style>
+
+
